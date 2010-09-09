6c051d0e9db332ed0f5511c6efa17bd056f7d9d5
chad <none@none>
2010-09-08 18:57:28 -0700

    setpkg: further refinement of low level setup scripts
    --HG--
    branch : sepkg_revamp
    rename : bin/lspkg.bat => bin/pkg.bat

--- bin/pkg.bat
+++ bin/pkg.bat
@@ -1,5 +1,5 @@
 
-@python.exe %~dp0\setpkgcli list %* > %TEMP%\temp.bat
+@python.exe %~dp0\setpkgcli %* > %TEMP%\temp.bat
 set /p TOOLOUTPUT= < %TEMP%\temp.bat
 %TEMP%\temp.bat
 del %TEMP%\temp.bat
---
+++ setpkg.sh
@@ -0,0 +1,29 @@
+
+set bin=`dirname $SETPKG_PATH`/bin
+
+function pkg { 
+	eval `$bin/setpkgcli --pid $$ set $SHELL "$@"`
+}
+
+function setpkg { 
+	pkg set "$@"
+}
+
+function unsetpkg { 
+	pkg unset "$@"
+}
+
+function runpkg {
+	setpkg "$@"
+	eval `pkginfo --exe "$@"`
+}
+
+function addenv { 
+	pkg env prepend "$@"
+}
+function delenv { 
+	pkg env pop "$@"
+}
+
+# system aliases
+pkg system-alias
