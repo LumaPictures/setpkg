9641799de6066e5b7fba3a2793b4797bb7840429
paulm <none@none>
2010-09-16 15:51:25 -0700

    setpkg: windows fix
    --HG--
    branch : sepkg_revamp

--- bin/setpkg.bat
+++ bin/setpkg.bat
@@ -1,5 +1,5 @@
 
-@python.exe %~dp0\setpkgcli set DOS %* > %TEMP%\temp.bat
+@python.exe %~dp0\setpkgcli --shell DOS set %* > %TEMP%\temp.bat
 set /p TOOLOUTPUT= < %TEMP%\temp.bat
 %TEMP%\temp.bat
 del %TEMP%\temp.bat
